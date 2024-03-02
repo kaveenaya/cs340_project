caf84b8c6bd51c2481df7139fd8ad4ebfe8d5243
ankithsridhar <ankithsridhar1221@gmail.com>
2024-03-01 16:11:17 -0800

    pushing update

--- app.js
+++ app.js
@@ -24,61 +24,60 @@ var db = require('./database/db-connector')
 /*
     ROUTES
 */
-// app.js
+
+// Define a route to handle the PUT request for updating a person's homeworld
+app.put('/put-person-ajax', function(req,res,next){
+    let data = req.body;
+
+    let homeworld = parseInt(data.homeworld);
+    let person = parseInt(data.fullname);
+
+    let queryUpdateWorld = `UPDATE bsg_people SET homeworld = ? WHERE bsg_people.id = ?`;
+    let selectWorld = `SELECT * FROM bsg_planets WHERE id = ?`;
+
+    // Run the 1st query
+    db.pool.query(queryUpdateWorld, [homeworld, person], function(error, rows, fields){
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {
+            // If there was no error, we run our second query and return that data so we can use it to update the people's table on the front-end
+            // Run the second query
+            db.pool.query(selectWorld, [homeworld], function(error, rows, fields) {
+                if (error) {
+                    console.log(error);
+                    res.sendStatus(400);
+                } else {
+                    res.send(rows);
+                }
+            })
+        }
+    });
+});
+
+// Remaining routes...
 
 // app.js
 
-/*
-    ROUTES
-*/                                                         
+app.get('/', function(req, res) {
+    res.render('home'); // Note the call to render() and not send(). Using render() ensures the templating engine will process this file, before sending the finished HTML to the client.
+});
 
-app.get('/shopping_cart', function (req, res) {
+// Define a route to handle the GET request for the '/instruments' endpoint
+app.get('/instruments', function(req, res) {
+    let query1 = "SELECT * FROM Instruments;"; // Define our query
 
-    res.render('shopping_cart');                  
-})
-                                                       // received back from the query
+    db.pool.query(query1, function(error, rows, fields) { // Execute the query
+        if (error) {
+            console.error("Error fetching instruments:", error);
+            res.status(500).send("Internal Server Error"); // Send internal server error status and message
+        } else {
+            res.render('instruments', { data: rows }); // Render the 'instruments.hbs' file and send the data to the template
+        }
+    });
+});
 
-app.get('/songs', function (req, res) {
-
-    res.render('songs');                  
-})
-
-app.get('/sales', function (req, res) {
-
-    res.render('sales');                  
-})
-
-app.get('/employees', function (req, res) {
-
-    res.render('employees');                  
-})
-
-
-app.get('/customers', function (req, res) {
-
-    res.render('customers');                  
-})
-
-app.get('/instruments', function(req, res)
-    {  
-        let query1 = "SELECT * FROM Instruments;";               // Define our query
-
-        db.pool.query(query1, function(error, rows, fields){    // Execute the query
-            //let instruments = rows;
-            res.render('instruments', {data: rows});                  // Render the index.hbs file, and also send the renderer
-        })                                                      // an object where 'data' is equal to the 'rows' we
-    });      
-    
-// app.js - ROUTES section
-
-// app.js
-
-app.get('/', function(req, res)
-    {
-        res.render('home');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.
-
-// app.js
 
 app.post('/add-instrument-form', function(req, res){
     // Capture the incoming data and parse it back to a JS object
@@ -86,47 +85,37 @@ app.post('/add-instrument-form', function(req, res){
 
     // Capture NULL values
     let id = parseInt(data['input-id']);
-    if (isNaN(id))
-    {
+    if (isNaN(id)) {
         id = 'NULL'
     }
 
     let year = parseInt(data['input-year']);
-    if (isNaN(year))
-    {
+    if (isNaN(year)) {
         year = 'NULL'
     }
 
     let price = parseInt(data['input-price']);
-    if (isNaN(price))
-    {
+    if (isNaN(price)) {
         price = 'NULL'
     }
 
     // Create the query and run it on the database
     query1 = `INSERT INTO Instruments (instrumentID, instrumentName, instrumentColor, instrumentMaterial, instrumentSize, instrumentYear, instrumentPrice) VALUES ('${id}', '${data['input-name']}', '${data['input-color']}', '${data['input-material']}', '${data['input-size']}', '${year}', '${price}')`;
     db.pool.query(query1, function(error, rows, fields){
-
         // Check to see if there was an error
         if (error) {
-
             // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
             console.log(error)
             res.sendStatus(400);
-        }
-
-        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
-        // presents it on the screen
-        else
-        {
+        } else {
             res.redirect('/');
         }
     })
-})
+});
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function() {
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
