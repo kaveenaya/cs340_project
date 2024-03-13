a110a70d76c293d94ec22dd01c992974f884b12a
preet545 <patelpreet414@gmail.com>
2024-03-13 14:21:39 -0700

    fixed the DDL

--- database/DDL.sql
+++ database/DDL.sql
@@ -1,105 +1,120 @@
--- Create tables
+-- Customers Table
 CREATE OR REPLACE TABLE Customers (
     customerID INT AUTO_INCREMENT PRIMARY KEY,
-    customerName VARCHAR(255),
+    customerFname VARCHAR(255),
+    customerLname VARCHAR(255),
     customerPhone VARCHAR(20),
-    customerDateOfPurchase DATE
+    customerEmail VARCHAR(255)
 );
 
+-- Employees Table
 CREATE OR REPLACE TABLE Employees (
     employeeID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
+    employeeFname VARCHAR(255),
+    employeeLname VARCHAR(255),
+    employeePhone VARCHAR(20),
+    employeeEmail VARCHAR(255)
 );
 
-CREATE OR REPLACE TABLE Sales (
-    salesID INT AUTO_INCREMENT PRIMARY KEY,
-    customerID INT,
-    employeeID INT,
-    saleAmount DECIMAL(10, 2),
-    saleDate DATE,
-    FOREIGN KEY (customerID) REFERENCES Customers(customerID),
-    FOREIGN KEY (employeeID) REFERENCES Employees(employeeID)
-);
-
-CREATE OR REPLACE TABLE ShoppingCart (
-    shoppingCartID INT AUTO_INCREMENT PRIMARY KEY,
-    salesID INT,
-    itemType VARCHAR(255),
-    itemID INT,
-    itemQuantity INT,
-    itemPrice DECIMAL(10, 2),
-    itemTotalPrice DECIMAL(10, 2),
-    FOREIGN KEY (salesID) REFERENCES Sales(salesID)
-);
-
-CREATE OR REPLACE TABLE Instruments (
-    instrumentID INT AUTO_INCREMENT PRIMARY KEY,
-    instrumentName VARCHAR(255) NOT NULL,
-    instrumentColor VARCHAR(255) NOT NULL,
-    instrumentMaterial VARCHAR(255) NOT NULL,
-    instrumentSize VARCHAR(255) NOT NULL,
-    instrumentYear INT NOT NULL,
-    instrumentPrice DECIMAL(10, 2) NOT NULL
-);
-
+-- Songs Table
 CREATE OR REPLACE TABLE Songs (
     songID INT AUTO_INCREMENT PRIMARY KEY,
     songName VARCHAR(255),
     songArtist VARCHAR(255),
     songGenre VARCHAR(255),
     songLength TIME,
-    songYear INT
+    songYear INT,
+    songPrice DECIMAL(10, 2)
+);
+
+-- Instruments Table
+CREATE OR REPLACE TABLE Instruments (
+    instrumentID INT AUTO_INCREMENT PRIMARY KEY,
+    instrumentName VARCHAR(255),
+    instrumentColor VARCHAR(255),
+    instrumentMaterial VARCHAR(255),
+    instrumentSize VARCHAR(50),
+    instrumentYear INT,
+    instrumentPrice DECIMAL(10, 2)
+);
+
+-- Sales Table
+CREATE OR REPLACE TABLE Sales (
+    salesID INT AUTO_INCREMENT PRIMARY KEY,
+    customerID INT,
+    employeeID INT,
+    saleAmount DECIMAL(10, 2),
+    saleDate DATE,
+    saleTime TIME,
+    FOREIGN KEY (customerID) REFERENCES Customers(customerID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (employeeID) REFERENCES Employees(employeeID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- ShoppingCart Table
+CREATE OR REPLACE TABLE ShoppingCart (
+    shoppingCartID INT AUTO_INCREMENT PRIMARY KEY,
+    salesID INT,
+    songID INT,
+    instrumentID INT,
+    itemQuantity INT,
+    itemTotalPrice DECIMAL(10, 2),
+    FOREIGN KEY (salesID) REFERENCES Sales(salesID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (songID) REFERENCES Songs(songID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (instrumentID) REFERENCES Instruments(instrumentID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
-
-INSERT INTO Instruments (instrumentID,instrumentName, instrumentColor, instrumentMaterial, instrumentSize, instrumentYear, instrumentPrice)
-VALUES 
-('2','Violin', 'Brown', 'Wood', 'Medium', 2023, 200.00),
-('3','Trumpet', 'Brass', 'Brass', 'Medium', 2023, 300.00),
-('4','Drums', 'Black', 'Wood', 'Large', 2023, 400.00);
-
--- Insert data into Songs table
-INSERT INTO Songs (songID, songName, songArtist, songGenre, songLength, songYear)
-VALUES
-(1, 'Bohemian Rhapsody', 'Queen', 'Rock', '00:05:55', 1975),
-(2, 'Billie Jean', 'Michael Jackson', 'Pop', '00:04:54', 1982),
-(3, 'Stairway to Heaven', 'Led Zeppelin', 'Rock', '00:08:02', 1971),
-(4, 'Back in Black', 'AC/DC', 'Rock', '00:04:15', 1980),
-(5, 'Sweet Child O'' Mine', 'Guns N'' Roses', 'Rock', '00:05:56', 1987);
-
 -- Insert data into Customers table
-INSERT INTO Customers (customerID, customerName, customerPhone, customerDateOfPurchase)
+INSERT INTO Customers (customerFname, customerLname, customerPhone, customerEmail)
 VALUES
-(123, 'Joe Doe', '123-422-3322', '2023-02-07'),
-(124, 'Jane Smith', '125-423-3324', '2023-02-08'),
-(125, 'Michale Doe', '124-424-3222', '2023-02-07');
+('Joe', 'Doe', '123-422-3322', 'joe@example.com'),
+('Jane', 'Smith', '125-423-3324', 'jane@example.com'),
+('Michael', 'Doe', '124-424-3222', 'michael@example.com');
 
 -- Insert data into Employees table
-INSERT INTO Employees (employeeID, name)
+INSERT INTO Employees (employeeFname, employeeLname, employeePhone, employeeEmail)
 VALUES
-(233321, 'Preet Patel'),
-(234322, 'Ankith Sridhar'),
-(235323, 'Kaveenaya Omkumar');
+('Preet', 'Patel', '123-456-7890', 'preet@example.com'),
+('Ankith', 'Sridhar', '234-567-8901', 'ankith@example.com'),
+('Kaveenaya', 'Omkumar', '345-678-9012', 'kaveenaya@example.com');
+
+
+-- Insert data into Songs table
+INSERT INTO Songs (songName, songArtist, songGenre, songLength, songYear, songPrice)
+VALUES
+('Bohemian Rhapsody', 'Queen', 'Rock', '00:05:55', 1975, 5.99),
+('Billie Jean', 'Michael Jackson', 'Pop', '00:04:54', 1982, 4.99),
+('Stairway to Heaven', 'Led Zeppelin', 'Rock', '00:08:02', 1971, 6.99),
+('Back in Black', 'AC/DC', 'Rock', '00:04:15', 1980, 4.99),
+('Sweet Child O'' Mine', 'Guns N'' Roses', 'Rock', '00:05:56', 1987, 5.99);
+
+
+-- Insert data into Instruments table
+INSERT INTO Instruments (instrumentName, instrumentColor, instrumentMaterial, instrumentSize, instrumentYear, instrumentPrice)
+VALUES 
+('Violin', 'Brown', 'Wood', 'Medium', 2023, 200.00),
+('Trumpet', 'Brass', 'Brass', 'Medium', 2023, 300.00),
+('Drums', 'Black', 'Wood', 'Large', 2023, 400.00);
 
 -- Insert data into Sales table
-INSERT INTO Sales (salesID, customerID, employeeID, saleAmount, saleDate)
+INSERT INTO Sales (customerID, employeeID, saleAmount, saleDate, saleTime)
 VALUES
-(1233, 123, 233321, 100, '2023-02-07'),
-(1244, 124, 234322, 200, '2023-02-08'),
-(1255, 125, 235323, 300, '2023-02-07');
+(1, 1, 100.00, '2023-02-07', '12:00:00'),
+(2, 2, 200.00, '2023-02-08', '13:00:00'),
+(3, 3, 300.00, '2023-02-07', '14:00:00');
 
 -- Insert data into ShoppingCart table
-INSERT INTO ShoppingCart (shoppingCartID, salesID, itemType, itemID, itemQuantity, itemPrice, itemTotalPrice)
+INSERT INTO ShoppingCart (salesID, songID, instrumentID, itemQuantity, itemTotalPrice)
 VALUES
-(12331, 1233, 'Drake', 1014, 2, 100.00, 200.00),
-(12441, 1244, 'Khalid', 1064, 3, 20.00, 60.00),
-(12551, 1255, 'Travis', 1078, 5, 60.00, 300.00);
+(1, 1, 1, 2, 5.99 * 2),
+(2, 2, 2, 3, 4.99 * 3),
+(3, 3, 3, 5, 6.99 * 5);
 
 -- Query data
-SELECT * FROM Instruments;
-SELECT * FROM Songs;
+
 SELECT * FROM Customers;
 SELECT * FROM Employees;
+SELECT * FROM Songs;
+SELECT * FROM Instruments;
 SELECT * FROM Sales;
 SELECT * FROM ShoppingCart;
