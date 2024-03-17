77c52bcd7ce935daab8386eff04e2b12099ed422
ankithsridhar <ankithsridhar1221@gmail.com>
2024-03-16 20:28:20 -0700

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -13,9 +13,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'Your ONID here',
-    password        : 'Your Password here',
-    database        : 'Your ONID here'
+    user            : 'cs340_srinivak',
+    password        : '9558',
+    database        : 'cs340_srinivak'
 })
 
 // Export it for use in our application
